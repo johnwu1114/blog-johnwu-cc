b5c1ee2a17e640c9af05dde467b8b8a151438409
John Wu <me@johnwu.cc>
2017-04-08 03:10:39 +0800

    Optimize static content.

---
+++ themes/next/source/js/bundle.js
@@ -0,0 +1,14 @@
+// vendors
+//require("../lib/jquery/index.js");
+require("../lib/fastclick/lib/fastclick.min.js");
+require("../lib/jquery_lazyload/jquery.lazyload.js");
+require("../lib/velocity/velocity.min.js");
+require("../lib/velocity/velocity.ui.min.js");
+
+// commons
+require("./src/utils.js");
+require("./src/motion.js");
+require("./src/custom.js");
+
+// bootstrap
+require("./src/bootstrap.js");
