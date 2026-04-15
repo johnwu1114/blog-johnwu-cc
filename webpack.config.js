c114e9c1bc2ac007a0fed05dab21c6edbc7d9b19
John Wu <me@johnwu.cc>
2026-04-15 22:06:16 +0800

    fix: build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-var HappyPack = require("happypack");
 var Webpack = require("webpack");
 var wwwroot = __dirname + "/themes/material-flow/source";
 
@@ -18,13 +17,13 @@ module.exports = {
         loaders: [{
             test: /\.js$/,
             exclude: [/node_modules/],
-            loader: "happypack/loader"
+            loader: "babel-loader",
+            query: {
+                presets: ["es2015"]
+            }
         }]
     },
     plugins: [
-        new HappyPack({
-            loaders: ["babel-loader?presets[]=es2015"]
-        }),
         new Webpack.optimize.UglifyJsPlugin(),
         new Webpack.ProvidePlugin({
             $: wwwroot + "/js/lib/jquery-2.2.4.min.js",
